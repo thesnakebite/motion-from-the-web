99fb0b3d7214c119b266f248150a223f74bcc88a
Álvaro Navarro <alvaro@thesnakebite.es>
2025-12-23 16:30:37 +0100

    transition timing Functions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 <x-layout stylesheet="css-transitions.css">
     <div class="wrapper">
         <div class="square"></div>
+
+        <div class="container">
+            <div class="box box--1"></div>
+            <div class="box box--2"></div>
+            <div class="box box--3"></div>
+            <div class="box box--4"></div>
+        </div>
     </div>
 </x-layout>
