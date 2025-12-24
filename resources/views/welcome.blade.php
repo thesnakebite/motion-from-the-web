ed1d0f11165b3254b9bf5bc0c623001e66f8ecce
Álvaro Navarro <alvaro@thesnakebite.es>
2025-12-24 17:53:22 +0100

    combined timing functions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
         <div class="square"></div>
 
         <div class="container">
+            {{--
             <div class="box box--1"></div>
             <div class="box box--2"></div>
             <div class="box box--3"></div>
             <div class="box box--4"></div>
+            --}}
         </div>
     </div>
 </x-layout>
